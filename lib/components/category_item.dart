21a937e5b0377e62bbb6353f0da462a927506c73
Yuri Garcia Ribeiro <81641949+YuriGarciaRibeiro@users.noreply.github.com>
2022-05-04 15:16:32 -0300

    iniciando projeto
    projeto iniciado, criada a tela inicial de categorias, e aplicados alguns estilos

---
+++ lib/components/category_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+
+  const CategoryItem({required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(category.title),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.blue,
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.5),
+            category.color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+    );
+  }
+}
