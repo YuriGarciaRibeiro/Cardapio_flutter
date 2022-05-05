6ed162acfb4e6894f8c5783ab087dba0f5048bd0
Yuri Garcia Ribeiro <81641949+YuriGarciaRibeiro@users.noreply.github.com>
2022-05-05 14:35:48 -0300

    colocando os alimentos em cada categoria

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,23 +1,28 @@
-// ignore_for_file: empty_constructor_bodies
+// ignore_for_file: empty_constructor_bodies, unused_local_variable, unrelated_type_equality_checks, missing_return
 
 import 'package:flutter/material.dart';
+import '../components/meal_item.dart';
 import '../models/category.dart';
+import '../data/dummy_data.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
-    final category = ModalRoute.of(context).settings.arguments as Category; 
+    final category = ModalRoute.of(context).settings.arguments as Category;
 
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(category.title),
-      ),
-      body: Center(
-        child: Text('Meals'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(category.title),
+        ),
+        body: ListView.builder(
+            
+            itemCount: categoryMeals.length,
+            itemBuilder: (ctx, index) {
+              return MealItem(meal: categoryMeals[index]);
+            }));
   }
 }
